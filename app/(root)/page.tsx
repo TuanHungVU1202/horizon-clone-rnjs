07610f5b7a911d23ae06ae7a22e27103f7bb70f9
TuanHungVU1202 <tuanhung.vu122@gmail.com>
2024-08-18 21:55:55 +0300

    Implement right side bard with placeholders for bank cards array

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,30 +1,37 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 function Home() {
-    const loggedIn = {firstName: "DUDE"};
+    const loggedIn = { firstName: "Dude", lastName: "No Left", email: "xyz@icloud.com" };
 
-  return (
-    <section className='home'>
-        <div className='home-content'>
-            <header className='home-header'>
-                <HeaderBox
-                    type="greeting"
-                    title="Welcome"
-                    user={loggedIn?.firstName || 'Guest'}
-                    subtext="Access and manage your account"
-                />
+    return (
+        <section className='home'>
+            <div className='home-content'>
+                <header className='home-header'>
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Access and manage your account"
+                    />
 
-                <TotalBalanceBox
-                    accounts={[]}
-                    totalBanks={1}
-                    totalCurrentBalance={9999.123}
-                />
-            </header>
-        </div>
-    </section>
-  )
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={9999.123}
+                    />
+                </header>
+            </div>
+
+            <RightSidebar 
+                user={loggedIn}
+                transacetions={[]}
+                banks={[ {}, {}]}
+            />
+        </section>
+    )
 }
 
 export default Home
